a7e45d60ec1c5d7f775b26ccc432da2842682c5e
patrickmugish <patrickmugisha911@>
2024-03-13 11:07:12 +0200

    Learning 02 activity done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Bartender";
+        job1._company = "Umwero";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Mechanization Lead";
+        job2._company = "Pride Farms";
+        job2._startYear = 2021;
+        job2._endYear = 2023;
+
+        Job job3 = new Job();
+        job3._jobTitle = "Mechanization Lead";
+        job3._company = "Pride Farms";
+        job3._startYear = 2023;
+        job3._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mugisha Patrick";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._jobs.Add(job3);
+
+        myResume.Display();
     }
 }
